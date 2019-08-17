a3bd4bb7b11ced1e4634cb643d05545760b91e89
leonk <leonklute@live.nl>
2019-08-17 12:12:15 +0200

    mqtt now fully implemented

--- main.py
+++ main.py
@@ -13,20 +13,22 @@ def on_message(client, userdata, message):
     print("message topic=", message.topic)
     print("message qos=", message.qos)
     print("message retain flag=", message.retain)
-    robot_messages.ParseFromString(message.payload)
+    if message.topic == "RoboFIFA/packaging":
+        print("parsing packaging message...")
+        robot_messages.ParseFromString(message.payload)
 
 
 def main():
     broker_address = "127.0.0.1"
     print("creating new instance")
-    client = mqtt.Client("P2")  # create new instance
+    client = mqtt.Client("packaging")  # create new instance
     client.on_message = on_message  # attach function to callback
     print("connecting to broker")
     client.connect(broker_address, port=1883)  # connect to broker
     client.loop_start()  # start the loop
     print("Subscribing to topic", "RoboFIFA/packaging")
     client.subscribe("RoboFIFA/packaging")
-    client.subscribe("esp32/temperature")
+    client.subscribe("RoboFIFA/feedback/+")
     #
     # try:
     #     with open('robot_wheel_speeds', "rb") as f:
@@ -48,6 +50,7 @@ def main():
                 msg = json.dumps(msg)
                 print(msg)
                 robot_server.send_to_robot(robot_message.id, msg)
+                client.publish("RoboFIFA/robot0", str(robot_message.left))
     except KeyboardInterrupt:
         pass
     print("closing robot server")
